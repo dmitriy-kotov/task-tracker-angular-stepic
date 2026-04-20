44196389f2a0024f74393d4ddd35e3941079e746
kotov <kotov_dl@nartis.ru>
2026-04-20 18:55:18 +0300

    feat: create-task component has been changed

--- src/app/entities/modal/create-task/create-task.component.ts
+++ src/app/entities/modal/create-task/create-task.component.ts
@@ -1,12 +1,79 @@
-import { Component } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { Task } from '../../../interface/task/task';
+import {
+  MAT_DIALOG_DATA,
+  MatDialogRef,
+  MatDialogModule,
+} from '@angular/material/dialog';
+import { Store } from '@ngrx/store';
+import { selectAllTasks } from '../../../feature/task/store/selector';
+import { addTaskAction } from '../../../feature/task/store/action';
+import { v4 as uuidv4 } from 'uuid';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-create-task',
   standalone: true,
-  imports: [],
+  imports: [
+    MatFormFieldModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatDialogModule,
+    MatButtonModule,
+  ],
   templateUrl: './create-task.component.html',
-  styleUrl: './create-task.component.scss'
+  styleUrl: './create-task.component.scss',
 })
-export class CreateTaskComponent {
+export class CreateTaskComponent implements OnInit {
+  form!: FormGroup;
+  tasks: Task[] = [];
 
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    private dialogRef: MatDialogRef<CreateTaskComponent>,
+    private fb: FormBuilder,
+    private store: Store
+  ) {}
+
+  ngOnInit(): void {
+    this.form = this.fb.group({
+      title: ['', [Validators.required, Validators.maxLength(255)]],
+      description: ['', [Validators.required, Validators.maxLength(255)]],
+    });
+
+    this.store.select(selectAllTasks).subscribe((myTasks) => {
+      this.tasks = myTasks;
+    });
+  }
+
+  onCancel(): void {
+    this.dialogRef.close();
+  }
+
+  onCreate(): void {
+    if (this.form.valid) {
+      const currentTasks = this.tasks;
+      const timeCreated = new Date();
+
+      const newTask: Task = {
+        id: uuidv4(),
+        title: this.form.value.title.trim(),
+        description: this.form.value.description.trim(),
+        columnId: 'todo: columnId',
+        timeCreate: timeCreated,
+        deadlineTime: timeCreated,
+      };
+
+      this.store.dispatch(addTaskAction({ task: newTask }));
+      this.dialogRef.close();
+    }
+  }
 }
